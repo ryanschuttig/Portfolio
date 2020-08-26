a03df12cd3ac352943a8905b4cfbc508bf7cf674
ryanschuttig <ryanschuttig@gmail.com>
2020-08-26 01:30:52 -0400

    email

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,7 +56,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" action="mailto:ryanschuttig@gmail.com">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
